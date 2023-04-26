b18909d1d570e300e07b1d33ec9c62b73e621e57
markjericho <jerichodon1998@gmail.com>
2023-04-26 23:20:53 +0800

    api url

--- src/pages/api/axiosInstance.js
+++ src/pages/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "http://localhost:1337/",
+	baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:1337/",
 	withCredentials: true,
 });
